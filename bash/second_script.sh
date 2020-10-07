a207a605506b28d9ad6ae03e4fd129e46f55cf00
Shin seongsu <tlstjdtn321@naver.com>
2020-10-07 23:27:21 +0900

    스크립트 세션

---
+++ bash/second_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 이 스크립트는 두 번째 스크립트입니다.
+# 스크립트명 : second_script.sh
+# 작성자 : Shin SS
+
+echo "$LOGNAME님 안녕하세요."
+echo "현재 디렉토리는 'pwd' 입니다."
+echo "접속해있는 시스템명은 'uname -n' 입니다."
+echo "현재 디렉터리의 파일 목록은 아래와 같습니다."
+ls
+echo "현재시간 : 'date'"
